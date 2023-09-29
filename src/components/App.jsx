8023405ec13671469e2a94146bacd69f0e14b00f
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-09-29 15:50:16 +0200

    switched to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -59,3 +59,66 @@ export const App = () => {
     </Container>
   );
 };
+
+//* ====== Old code before transitioning to hooks from class components. ======
+// import { Container } from './index.styled';
+// import { Section } from './Section/Section';
+// import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+// import { Component } from 'react';
+// import { Statistics } from './Statistics/Statistics';
+// import { Notification } from './Notification/Notification';
+
+// export class App extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
+
+//   handlerFeedback = (event, option) => {
+//     this.setState(prevState => {
+//       return {
+//         [option]: prevState[option] + 1,
+//       };
+//     });
+//   };
+
+//   countTotalFeedback = (good, neutral, bad) => good + neutral + bad;
+
+//   countPositiveFeedbackPercentage(good, total) {
+//     return Math.round((good / total) * 100);
+//   }
+
+//   render() {
+//     const { good, neutral, bad } = this.state;
+//     const total = this.countTotalFeedback(good, neutral, bad);
+//     const positivePercentage = this.countPositiveFeedbackPercentage(
+//       good,
+//       total
+//     );
+
+//     return (
+//       <Container>
+//         <Section title="We'd love your feedback">
+//           <FeedbackOptions
+//             options={Object.keys(this.state)}
+//             onLeaveFeedback={this.handlerFeedback}
+//           />
+//         </Section>
+//         <Section title="Statistics">
+//           {total ? (
+//             <Statistics
+//               good={good}
+//               neutral={neutral}
+//               bad={bad}
+//               total={total}
+//               positivePercentage={positivePercentage}
+//             />
+//           ) : (
+//             <Notification message="There is no feedback" />
+//           )}
+//         </Section>
+//       </Container>
+//     );
+//   }
+// }
